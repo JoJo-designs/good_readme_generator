7f662a3a90da565e49be949896b1573280798383
jojo <jodiladouceur.design@gmail.com>
2021-04-16 14:40:16 -0400

    edits to index.js

--- index.js
+++ index.js
@@ -1,79 +1,103 @@
-// This file will use node to run outside a browser I need to create a program that will make a good readme file based on user 
-//imputs. Will promtped user for information and than fill in the data to the readme file under the correct section titles
-
-
-// Sections that needs to be added
-// Project Title:       The tile at the top of the readme
-                        // Licence badge is near here at the top
-// Description:         About the project 
-// table of content:    When clicked will bring user to the corasponding part of the file
-// Installation:        Instuctions on how to install
-// Usage:               Info on how to use application
-// License:             Explains the license the project is listed under.
-// Contributing:        Info on contriduting to a project
-// Tests:               Info about testing
-// Questions:           contains a link to the users github
-                        // Github user name and email with instraction to reach the user for more info
-
-
-//Using propmts
-
-const prompted = require('prompt');
-const prompting = require('prompt-sync')({sigint: true});
+// Required files
 const createfile = require('fs');
+const inqui = require('inquirer');
 
-    let title;
-    let gitHubUserName;
-    let emailAddress;
-    let description;
-    let installation;
-    let usage;
-    let license;
-    let contributing;
-    let test;
-    let question;
+// Need a way to make some standard text appear with the license.
+let type;
+const CC0 = "copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission"
+const Apache20 = "This is a type of license2. is auto text that is added in the sections based on the licence that is selected"
+const ctype = "This is a type of license3. is auto text that is added in the sections based on the licence that is selected"
+const dtype = "This is a type of license4. is auto text that is added in the sections based on the licence that is selected"
+
+inqui
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project? ',
+      name: 'projectTitle',
+    },
+    {
+      type: 'input',
+      message: 'What is your Github user name? ',
+      name: 'gitHub',
+    },
+    {
+      type: 'input',
+      message: 'What is your email? ',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'What does your program do? ',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'How do people install the application? ',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'How do people use this application? ',
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      message: 'which license is this application under? ',
+      choices: ["CC0", "Apache-2.0", "c", "d"],
+      name: 'license',
+      //when: (answers) => answers.isCc0 === CCO,
+    },
+    {
+      type: 'input',
+      message: 'How do people contribute to the project? ',
+      name: 'contributing',
+    },
+    {
+      type: 'input',
+      message: 'How is testing done? ',
+      name: 'testing',
+    }, 
+  ])
 
 
-// prompted.start();
-
-// prompted.get(["Git Hub User Name", "email Address"], function(err, result){
-//     if (err) {return onError(err);}
-//     console.log(result)
-// })
-
-// function onError(err){
-//     console.log(err);
-//     return 1;
-// }
-
-// prompted for info
-//Ask for a name first not nessasary but a firendly welcome
-userWelecome = prompting('What is your name? ');
-      console.log(`Welcome ${userWelecome} to this porfessonal readme generator. 
-      Fill in the information as prompted to build a professonal README.md file`)
-//Rest of the questions
-      title = prompting('What is the title of your project? ');
-      gitHubUserName = prompting('What is your Github user name? ');
-      emailAddress = prompting('What is your email? ')
-      description = prompting('What does your program do? ');
-console.log("Generating File...")
-
-
-// builds file with data given by the user.
-createfile.writeFile('log.md', `<h1>${title}</h1>\n
-<h3>description</h3>\n
-${description}\n
-<h3>table of content</h3>\n
-<ul>
-<li>Installation</li>\n<li>Usage</li>\n<li>License</li>\n<li>Contributing</li>\n<li>Tests</li>\n<li>Questions</li>
-</ul>\n
-<h3>Installation</h3>\n
-<h3>Usage</h3>\n
-<h3>Licence</h3>\n
-<h3>Conributing</h3>\n
-<h3>Test</h3>\n
-<h3>Questions</h3>\n`, (err) =>
-  err ? console.error(err) : console.log('file generated')
+  .then((data) =>
+  createfile.writeFile('Read.md', `# ${data.projectTitle}
+  ### Description
+  ${data.description}
+  ### Table of Content
+  [Installation](href='#Installation')
+  [Usage](href='#Usage')
+  [License](href='#License')
+  [Contributing](href='#Contributing')
+  [Testing](href='#Testing')
+  [Questions](href='#Questions')
+  ### Installation
+  ${data.installation}
+  ### Usage  
+  ${data.usage}
+  ### License
+  ##### Type of license ${data.license}
+  Projects with the ${data.license} allow users to 
+  ### Contributing
+  ${data.contributing}
+  ### Test
+  ${data.testing}
+  ### Questions
+  Github: ${data.gitHub}
+  Please contact ${data.email} with any questions`, (err) =>
+  err ? console.error(err) : console.log('complete')
+)
 );
 
+var checktype = (data) => {
+  if (data.license === "CC0") {
+    type = "copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission"
+  } else if (data.license === "Apache20") {
+    type = Apache20
+  }
+}
+
 //Cheat sheet for markdown stuff might be useful https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#headers
+// links https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+
